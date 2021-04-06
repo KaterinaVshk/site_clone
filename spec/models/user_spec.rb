3cbe20a367ed890b72f76e4676d41652c0a57c33
Vashkevich Ekaterina <veo_25@mail.ru>
2021-04-07 01:40:22 +0300

    Fix rubocop errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe(User, type: :model) do
     describe '#email' do
       it 'when invalid email' do
         user1.email = 'jfbui'
-        expect(subject).not_to(be_valid)
+        expect(user1).not_to(be_valid)
       end
 
       it { is_expected.to(validate_presence_of(:email).with_message('Укажите email')) }
@@ -20,7 +20,7 @@ RSpec.describe(User, type: :model) do
       it { is_expected.to(validate_presence_of(:password).with_message('Пароль обязательный')) }
 
       it {
-        expect(subject).to(validate_length_of(:password).is_at_least(8).with_message('Пароль должен быть от 8 до 64 символов'))
+        expect(user1).to(validate_length_of(:password).is_at_least(8).with_message('Пароль должен быть от 8 до 64 символов'))
       }
     end
   end
