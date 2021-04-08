8994cd604a29cde1ef216849de0504049721ef67
Vashkevich Ekaterina <veo_25@mail.ru>
2021-04-08 18:13:24 +0300

    Create categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_07_231507) do
+ActiveRecord::Schema.define(version: 2021_04_08_133159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
