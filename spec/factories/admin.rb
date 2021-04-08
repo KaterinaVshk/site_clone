692ee753290c4e42a86d1236971e524f21956379
Vashkevich Ekaterina <veo_25@mail.ru>
2021-04-08 13:22:45 +0300

    Fix style code errors

--- spec/factories/admin.rb
+++ spec/factories/admin.rb
@@ -1,8 +1,7 @@
 FactoryBot.define do
-    factory :admin do
-        email { 'admin@mail.ru' }
-        password { '11111111' }
-        password_confirmation { '11111111' }
-    end
+  factory :admin do
+    email { 'admin@mail.ru' }
+    password { '11111111' }
+    password_confirmation { '11111111' }
+  end
 end
-  
