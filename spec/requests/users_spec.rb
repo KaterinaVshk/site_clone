d94538ce8eb0e22cca02538a129b146ed17ac2c1
Vashkevich Ekaterina <veo_25@mail.ru>
2021-04-19 16:53:41 +0300

    Add admin creation

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe('Users', type: :request) do
-  let(:user1) { create(:user) }
+  let!(:user1) { create(:user) }
+  let!(:admin)       { create(:admin)                                   }
+  let(:admin_params) { { email: admin.email, password: admin.password } }
+  let(:user_params)  {  { email: user1.email, password: user1.password }  }
+
+
+  describe '#index' do
+    it 'receives all users when admin' do
+      post '/login', params: admin_params
+      get '/users'
+      expect(assigns(:users)).to(eq(User.all))
+    end
+
+    it 'redirect to login path if not admin' do
+      post '/login', params: user_params
+      get '/users'
+      expect(response).to(
+        redirect_to(
+          root_path
+        )
+      )
+    end
+  end
 
   describe '#create' do
     context 'with valid attributes' do
@@ -35,4 +57,59 @@ RSpec.describe('Users', type: :request) do
       end
     end
   end
+
+  describe '#make_admin' do
+    context 'when admin' do
+      it 'updates user and redirect to index path' do
+        post '/login', params: admin_params
+        put "/make_admin/#{user1.id}"
+        expect(User.find(user1.id).type).to(eql('Admin'))
+        expect(response).to(
+          redirect_to(
+            action: :index
+          )
+        )
+      end
+    end
+
+    context 'when not admin' do
+      it 'redirect to root path ' do
+        post '/login', params: user_params
+        put "/make_admin/#{user1.id}"
+        expect(response).to(
+          redirect_to(
+            root_path
+          )
+        )
+      end
+     end
+  end
+
+  describe '#cancel_admin_rights' do
+    context 'when admin' do
+      it 'cancel admin rights and redirect to index path' do
+        post '/login', params: admin_params
+        put "/cancel_admin/#{user1.id}"
+        expect(User.find(user1.id).type).to(eql('User'))
+        expect(response).to(
+          redirect_to(
+            action: :index
+          )
+        )
+      end
+    end
+
+    context 'when not admin' do
+      it 'redirect to root path ' do
+        post '/login', params: user_params
+        put "/cancel_admin/#{user1.id}"
+        expect(response).to(
+          redirect_to(
+            root_path
+          )
+        )
+      end
+     end
+  end
+
 end
